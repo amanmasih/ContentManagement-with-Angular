5f8e9068d530c52bd833410dd243fc2be517e65e
AMAN MASIH <39672588+amanmasih@users.noreply.github.com>
2022-03-08 22:58:23 +0530

    Delete ContentManagement directory

--- ContentManagement/authenticationservice/src/main/java/com/niit/authenticationservice/rabbitmq/UserDTO.java
+++
@@ -1,9 +0,0 @@
-package com.niit.authenticationservice.rabbitmq;
-import lombok.Data;
-
-@Data
-public class UserDTO {
-    private String userId;
-    private String userName;
-    private String password;
-}
--- ContentManagement/usercontent/src/main/java/com/niit/usercontent/Exception/UserNotFound.java
+++
@@ -1,8 +0,0 @@
-package com.niit.usercontent.Exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND,reason = "User not found")
-public class UserNotFound extends Exception{
-}
